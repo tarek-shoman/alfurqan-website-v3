44ee713ee5b15f5fe40490d38263548a9f5bf370
ElshefaaPharmacy <elshefaa@example.com>
2026-01-03 00:55:25 +0200

    Deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/alfurqan-website-v2/',
+  base: '/alfurqan-website-v3/',
   plugins: [react()],
 });
